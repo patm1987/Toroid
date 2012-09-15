fb8f3b9757494f3506f1ac72451c4aefa1410bad
Patrick Martin <pux1987@gmail.com>
2012-09-15 14:30:27 -0600

    Filled out the event system
    Soon to test the list and event system

--- Toroid/Toroid/messaging/messaging.c
+++ Toroid/Toroid/messaging/messaging.c
@@ -47,11 +47,31 @@ void MessageDataDestructor(void* pData, void* pUserData)
 	SAFE_FREE(pData);
 }
 
+/*!
+ * \brief	comparator for MessageData
+ * \param	pLhs a MessageData_t pointer for the left hand side of the comparison
+ * \param	pRhs a MessageData_t pointer for the right hand side of the comparison
+ * \param	pUserData - ignored
+ * \return	0 if pLhs == pRhs, else return 1
+ * \note	two MessageData_t's are equal iff their listener and user data portions both point
+ *			to the same memory address
+ */
 int MessageDataComparator(void* pLhs, void* pRhs, void* pUserData)
 {
-	// TODO: compare pLhs and pRhs
+	struct MessageData_t* pMsgLeft = (struct MessageData_t*)pLhs;
+	struct MessageData_t* pMsgRight = (struct MessageData_t*)pRhs;
+
+	if (pMsgLeft->listener == pMsgRight->listener
+		&& pMsgLeft->pUserData == pMsgRight->pUserData)
+	{
+		return 0;
+	}
+	return 1;
 }
 
+/*!
+ * \brief	initialize the Messaging system
+ */
 void Messaging_Init()
 {
 	int index;
@@ -61,6 +81,9 @@ void Messaging_Init()
 	}
 }
 
+/*!
+ * \brief	cleanup the Messaging system
+ */
 void Messaging_Cleanup()
 {
 	int index;
@@ -71,17 +94,36 @@ void Messaging_Cleanup()
 	}
 }
 
+/*!
+ * \brief	adds a \a listener for the given message
+ * \param	listener the callback to call when \a msg is raised
+ * \param	pUserData user data sent to the \a listener 's pUserData parameter
+ * \param	msg the message this \a listener will listen for
+ */
 void Messaging_AddListener(Messaging_Listener listener, void* pUserData, message msg)
 {
 	struct MessageData_t *pMessageData = ALLOCATE(struct MessageData_t);
 	List_add(s_listeners[msg], pMessageData);
 }
 
+/*!
+ * \brief	removes a \a listener for the given \a msg
+ * \param	listener the callback for the listener that will be removed
+ * \param	pUserData user data sent to the \a listener 's pUserData parameter, this MUST match
+ *			the memory address of the original user data or the listener will NOT be removed
+ * \param	msg the message the \a listener was listening for
+ */
 void Messaging_RemoveListener(Messaging_Listener listener, void* pUserData, message msg)
 {
-	// TODO: ... yeah
+	struct MessageData_t msgData = {listener, pUserData};
+	List_removeCmp(s_listeners[msg], &msgData, MessageDataComparator, NULL);
 }
 
+/*!
+ * \brief	raises a message with associated user data
+ * \param	msg the message to raise
+ * \param	pData data sent to each listener's pData parameter
+ */
 void Messaging_RaiseMessage(message msg, void* pData)
 {
 	struct RaiseEventUserData_t data = {msg, pData};
