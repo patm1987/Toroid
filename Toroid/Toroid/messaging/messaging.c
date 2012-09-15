90b2fb1147c2448066e9046d30d686e2ea02aff4
Patrick Martin <pux1987@gmail.com>
2012-09-15 13:04:57 -0600

    added some implementation to messaging

--- Toroid/Toroid/messaging/messaging.c
+++ Toroid/Toroid/messaging/messaging.c
@@ -1,7 +1,10 @@
 
 #include "messaging.h"
 
+#include <stdlib.h>
+
 #include "../utils/list.h"
+#include "../utils/memoryUtils.h"
 
 struct RaiseEventUserData_t
 {
@@ -18,6 +21,8 @@ struct MessageData_t
 static List* s_listeners[message_count] = {0};
 
 void RaiseEventForEachCB(void* pData, void* pUserData);
+void MessageDataDestructor(void* pData, void* pUserData);
+int MessageDataComparator(void* pLhs, void* pRhs, void* pUserData);
 
 /*!
  * \brief	raises an event on each listener in a listener list
@@ -32,22 +37,49 @@ void RaiseEventForEachCB(void* pData, void* pUserData)
 	messageData->listener(pEventData->msg, messageData->pUserData, pEventData->pData);
 }
 
+/*!
+ * \brief	used for cleaning up the listener list
+ * \param	pData a MessageData_t to clean up
+ * \param	pUserData - ignored
+ */
+void MessageDataDestructor(void* pData, void* pUserData)
+{
+	SAFE_FREE(pData);
+}
+
+int MessageDataComparator(void* pLhs, void* pRhs, void* pUserData)
+{
+	// TODO: compare pLhs and pRhs
+}
+
 void Messaging_Init()
 {
-	// don't know if there's anything to do here... yet
+	int index;
+	for (index = 0; index < message_count; index++)
+	{
+		s_listeners[index] = List_Create();
+	}
 }
 
 void Messaging_Cleanup()
 {
-	// go over each event list and clean up the listeners
+	int index;
+	for (index = 0; index < message_count; index++)
+	{
+		List_DestroyWithDestructor(s_listeners[index], MessageDataDestructor, NULL);
+		s_listeners[index] = NULL;
+	}
 }
 
 void Messaging_AddListener(Messaging_Listener listener, void* pUserData, message msg)
 {
+	struct MessageData_t *pMessageData = ALLOCATE(struct MessageData_t);
+	List_add(s_listeners[msg], pMessageData);
 }
 
 void Messaging_RemoveListener(Messaging_Listener listener, void* pUserData, message msg)
 {
+	// TODO: ... yeah
 }
 
 void Messaging_RaiseMessage(message msg, void* pData)
